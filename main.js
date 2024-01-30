a8097bd8acdfc6c2aaf36e12ef2a11508de1a126
Harshal Gunjal <141690905+harshalgunjal69@users.noreply.github.com>
2024-01-30 08:36:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var toggleSwitch = document.getElementById("toggleSwitch");
+
+    // Set initial state based on saved value
+    chrome.storage.sync.get("extensionState", function (data) {
+        toggleSwitch.checked = data.extensionState || false;
+    });
+
+    // Toggle the extension state when the switch is changed
+    toggleSwitch.addEventListener("change", function () {
+        chrome.storage.sync.set({ extensionState: toggleSwitch.checked });
+    });
+});
